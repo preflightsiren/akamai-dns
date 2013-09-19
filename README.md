9522faa2adf672764dab9b04418d9789ad611f2f
unknown <sebastian.cole@D1290.9msn.net>
2013-09-19 13:29:10 +1000

    Added support README and support for env vars
    Added README.md
    Moved sitename to ENV['sitename']
    Moved domain to ENV['domain']
    added rspec formatting

---
+++ README.md
@@ -0,0 +1,22 @@
+#Akamai DNS tests
+
+Sticking to this convention makes it much easier to manage our sites.
+
+###Overview of tests
+
+Tests for well known entry-points (Akamai fronted, origin)
+redirection to www.
+multi region support
+
+###Running the specs
+to run with defaults run:
+> bundle install && bundle exec rspec
+
+
+
+to customise the execution set environment variables
+
+ENV['sitename'] => Default is 'now'
+ENV['domain'] => Default is 'ninemsn.com.au'
+
+> bundle install && bundle exec rspec
--- spec/akamai_spec.rb
+++ spec/akamai_spec.rb
@@ -2,29 +2,39 @@ require 'net/http'
 require 'rspec'
 require 'resolv'
 
+site_name = ENV['sitename'] || 'now'
+domain = ENV['domain'] || 'ninemsn.com.au'
 site_url = 'now.ninemsn.com.au'
 dns = Resolv::DNS.new(:nameserver => ['8.8.8.8']) 
 
 describe 'DNS conventions' do
   it 'has a apex zone publicly available' do
-   rr = dns.getaddresses(site_url)
+   rr = dns.getaddresses("#{site_name}.#{domain}")
    rr.should_not be_empty
   end
   it 'should have www CNAME\'d to edgesuite' do
-    rr = dns.getresources("www.#{site_url}", Resolv::DNS::Resource::IN::CNAME)
+    rr = dns.getresources("www.#{site_name}.#{domain}", Resolv::DNS::Resource::IN::CNAME)
     rr.should_not be_empty
     rr[0].name.to_s.should match('edgesuite.net')
   end
   it 'should have an origin record' do
-    rr = dns.getresources("now.origin.ninemsn.com.au", Resolv::DNS::Resource::IN::CNAME)
+    rr = dns.getresources("#{site_name}.origin.#{domain}", Resolv::DNS::Resource::IN::CNAME)
     rr.should_not be_empty
   end
   it 'should have a Sydney region record' do
-    rr = dns.getresources("now.ap2.ninemsn.com.au", Resolv::DNS::Resource::IN::CNAME)
+    rr = dns.getresources("#{site_name}.ap2.#{domain}", Resolv::DNS::Resource::IN::CNAME)
     rr.should_not be_empty
   end
   it 'should have a Singapore region record' do
-    rr = dns.getresources("now.ap1.ninemsn.com.au", Resolv::DNS::Resource::IN::CNAME)
+    rr = dns.getresources("#{site_name}.ap1.#{domain}", Resolv::DNS::Resource::IN::CNAME)
     rr.should_not be_empty
+  end
+end
+
+describe 'hosting site off www. resource' do
+  it 'should redirect to www.' do
+    uri = URI("http://#{site_name}.#{domain}")
+    response = Net::HTTP.get_response(uri)
+    response.class.should be(Net::HTTPMovedPermanently)
   end
 end
